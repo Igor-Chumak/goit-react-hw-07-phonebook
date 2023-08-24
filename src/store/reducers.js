a1edd2cd9fd24bc77c57d97c8435dd7e9c0c4595
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-24 14:05:32 +0300

    persist ok

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -6,7 +6,6 @@ import { modeThemeReducer } from './themeSlice';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { LOCAL_STORAGE_KEY } from 'store/constants';
-// import createTransform from 'redux-persist/es/createTransform';
 
 const persistConfigContacts = {
   key: LOCAL_STORAGE_KEY,
@@ -15,28 +14,10 @@ const persistConfigContacts = {
   blacklist: [],
 };
 
-// const SetTransform = createTransform(
-// transform state on its way to being serialized and persisted.
-// (inboundState, key) => {
-// convert mySet to an Array.
-// return { ...inboundState, mySet: [...inboundState] };
-// }
-
-// transform state being rehydrated
-// (outboundState, key) => {
-// convert mySet back to a Set.
-// return { ...outboundState, mySet: new Set(outboundState.mySet) };
-// }
-
-// define which reducers this transform gets called for.
-// { whitelist: ['someReducer'] }
-// );
-
 const persistConfigTheme = {
   key: LOCAL_STORAGE_KEY + '_theme',
   storage,
   // stateReconciler: autoMergeLevel2
-  // transforms: [SetTransform],
 };
 
 const persistedReducerTheme = persistReducer(
