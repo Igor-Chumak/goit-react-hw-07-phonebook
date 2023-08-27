be7ab59ba0db54461564afd6fafa85f4ca1267cb
Igor <125488812+Igor-Chumak@users.noreply.github.com>
2023-08-27 23:19:29 +0300

    logic sortby ok

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { SORT_BY } from './constants';
 
 export const selectContacts = state => state.contacts;
 export const selectFilter = state => state.filter;
@@ -6,10 +7,22 @@ export const selectModeTheme = state => state.theme.mode;
 export const selectSortBy = state => state.sortby;
 
 export const selectVisibleContacts = createSelector(
-  [selectContacts, selectFilter],
-  ({ items }, filter) => {
-    return items.filter(contact =>
+  [selectContacts, selectFilter, selectSortBy],
+  ({ items }, filter, sortby) => {
+    const filteredContacts = items.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
+    switch (sortby) {
+      case SORT_BY.AZ_SORT: {
+        console.log('AZ_SORT :>> ');
+        return filteredContacts.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      case SORT_BY.ZA_SORT:
+        console.log('ZA_SORT :>> ');
+        return filteredContacts.sort((a, b) => b.name.localeCompare(a.name));
+      default:
+        console.log('none sort :>> ');
+        return filteredContacts;
+    }
   }
 );
